f80ff610d2f7c4e2154cbbfe007500f91f4a963f
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2024-06-15 09:15:53 +0700

    fix pass content to css

--- e-commerce-customer/src/components/MessageIcon/MessageIcon.tsx
+++ e-commerce-customer/src/components/MessageIcon/MessageIcon.tsx
@@ -1,3 +1,5 @@
+import "./style.scss";
+
 export interface MessageIconProps {
   alt: string;
   icon: string;
@@ -8,7 +10,8 @@ export interface MessageIconProps {
 const MessageIcon = ({ alt, icon, className, number }: MessageIconProps) => {
   return (
     <span
-      className={`cursor-pointer flex items-center justify-center relative before:content-['${number}'] before:flex before:items-center before:justify-center before:absolute before:bg-red-1 before:text-white before:rounded-full before:w-5 before:h-5 before:text-sm before:bottom-[65%] before:left-[50%] ${className}`}
+      className={`message cursor-pointer flex items-center justify-center relative before:flex before:items-center before:justify-center before:absolute before:bg-red-1 before:text-white before:rounded-full before:w-5 before:h-5 before:text-sm before:bottom-[65%] before:left-[50%] ${className}`}
+      data-content={number}
     >
       <img src={icon} alt={alt} className="object-cover" />
     </span>
